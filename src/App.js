9b865a09abb073ec03fde2ed5151e3af3b230850
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2021-02-28 03:39:01 +0400

    003 Making input parsing

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
-import './App.scss';
-import './components/Task/Task.scss';
-import { useState, useEffect } from 'react';
-import { getFromLocalStorage, saveToLocalStorage } from './utils/localStorageMethods';
-import Task from './components/Task/Task';
-import calendarIcon from './components/Task/calendar-icon.png';
-import arrowIcon from './components/Task/down-arrow.png';
+import "./App.scss";
+import { useState, useEffect } from "react";
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from "./utils/localStorageMethods";
+import Task from "./components/Task/Task";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const tasks = getFromLocalStorage('tasks');
+    const tasks = getFromLocalStorage("tasks");
     if (tasks) setTasks(tasks);
   }, []);
+
+  function addBlankTaskHandler() {
+    setTasks([...tasks, <Task />]);
+  }
 
   return (
     <div className="app">
       <ul className="task-list">
-        {tasks.map((task) => (
-          <Task />
+        {tasks.map((task, index) => (
+          <Task key={index} />
         ))}
-        <li className="task task_type_template">
-          <input type="text" className="task__text-field task__text-field_type_in-template" placeholder="Write a new task" disabled />
-        </li>
-
-        <li className="task">
-          <form className="task__form">
-            <label className="task__checkbox">
-              <input type="checkbox" className="task__checkbox-proper" />
-              <span className="task__checkbox-pseudo"></span>
-            </label>
-
-            <input type="text" className="task__text-field task__text-field_type_task-name" placeholder="Write a new task"></input>
-
-            <button type="button" className="task__calendar-widget-opener">
-              <img src={calendarIcon} alt="Значок календаря" />
-            </button>
-
-            <label className="task__dropdown">
-              <span className="task__list-color"></span>
-              <input type="text" className="task__text-field task__text-field_type_list-name" placeholder="No list"></input>
-              <img className="task__dropdown-arrow" src={arrowIcon} alt="Стрелка раскрытия списка" />
-            </label>
-          </form>
-        </li>
+        <Task
+          key={tasks.length + 1}
+          isTemplate={true}
+          onClick={addBlankTaskHandler}
+        />
       </ul>
     </div>
   );
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,83 @@
 import './Task.scss';
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import calendarIcon from './calendar-icon.png';
+import arrowIcon from './down-arrow.png';
+import { saveToLocalStorage } from '../../utils/localStorageMethods';
 
-function Task() {
+function Task(props) {
+  const { isTemplate, onClick } = props;
+
+  const [inputStateValue, setInputStateValue] = useState('');
+
+  function handleInput(event) {
+    const { value } = event.target;
+    const myRegexp = /(.*)(\/\/)(.*)/;
+    const matchResult = myRegexp.exec(value);
+    let task = null;
+    let commentMark = null;
+    let commentValue = null;
+    if (matchResult) {
+      task = matchResult[1];
+      commentMark = matchResult[2];
+      commentValue = matchResult[3];
+    }
+    if (commentMark) {
+      setInputStateValue(`${task} COMMENT ${commentValue}`);
+      console.log('inputStateValue', inputStateValue);
+    // } else {
+    //   setInputStateValue(value);
+    }
+  }
+
+  function handleFieldChange(event) {
+    setInputStateValue(event.target.value);
+  }
 
   return (
-    <li className="task">
+    <li className={`task${isTemplate ? " task_type_template" : ""}`}>
+      {isTemplate ? (
+        <button
+          type="button"
+          className="task__add-task-button"
+          onClick={onClick}
+        >
+          Write a new task
+        </button>
+      ) : (
+        <form className="task__form">
+          <label className="task__checkbox">
+            <input type="checkbox" className="task__checkbox-proper" />
+            <span className="task__checkbox-pseudo"></span>
+          </label>
+
+          <input
+            type="text"
+            className="task__text-field task__text-field_type_task-name"
+            placeholder="Write a new task"
+            value={inputStateValue}
+            onChange={handleFieldChange}
+            onInput={handleInput}
+          />
+
+          <button type="button" className="task__calendar-widget-opener">
+            <img src={calendarIcon} alt="Значок календаря" />
+          </button>
+
+          <label className="task__dropdown">
+            <span className="task__list-color"></span>
+            <input
+              type="text"
+              className="task__text-field task__text-field_type_list-name"
+              placeholder="No list"
+            ></input>
+            <img
+              className="task__dropdown-arrow"
+              src={arrowIcon}
+              alt="Стрелка раскрытия списка"
+            />
+          </label>
+        </form>
+      )}
     </li>
   );
 }
